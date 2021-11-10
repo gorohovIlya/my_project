58e5e3c2375e75d4d657feec1d8aa2431870ca04
gorohovIlya <93247075+gorohovIlya@users.noreply.github.com>
2021-11-10 19:57:58 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,61 @@
+import DinoInfo
+import region
+import search_results
+import test
+import test_map
 import sys
 import sqlite3
-from PyQt5 import uic, QtCore
+from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QVBoxLayout, QTableWidgetItem, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QVBoxLayout, QLabel
 from PyQt5.QtGui import QPixmap
 
 connection = sqlite3.connect('dinos_db.db')
 cursor = connection.cursor()
 
+FLAGS = [['агрессивный', 'нейтральный', 'послушный'],
+         ['хищник', 'падальщик', 'травоядный'],
+         ['ездовое', 'неездовое'],
+         ]
 
-class MyArkManual(QMainWindow):
+
+class MyArkManual(QMainWindow, test.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.show()
         self.exit.clicked.connect(self.exit_from_app)
+        self.search_btn.clicked.connect(lambda: self.open_searcher_window(self.lineEdit.text()))
 
     def initUI(self):
-        cursor = connection.cursor()
-        rqst = cursor.execute('SELECT * FROM Maps').fetchall()
-        uic.loadUi('test.ui', self)
-        for el in rqst:
-            pixmap = QPixmap()
-            pixmap.loadFromData(el[2], "png")
-            pixmap_resized = pixmap.scaled(120, 120, QtCore.Qt.KeepAspectRatio)
-            btn = QPushButton(el[1], self)
-            btn.setObjectName(el[3])
-            label = QLabel()
-            label.setPixmap(pixmap_resized)
-            frame = QVBoxLayout()
-            frame.addWidget(label, alignment=Qt.AlignCenter)
-            frame.addWidget(btn)
-            if btn.text() == 'The Island':
-                btn.clicked.connect(lambda: self.open_map_window('The Island'))
-            elif btn.text() == 'Scorched Earth':
-                btn.clicked.connect(lambda: self.open_map_window('Scorched Earth'))
-            elif btn.text() == 'Aberration':
-                btn.clicked.connect(lambda: self.open_map_window('Aberration'))
-            elif btn.text() == 'Extinction':
-                btn.clicked.connect(lambda: self.open_map_window('Extinction'))
-            elif btn.text() == 'Ragnarok':
-                btn.clicked.connect(lambda: self.open_map_window('Ragnarok'))
-
-            self.MapButtons.addLayout(frame)
+        try:
+            rqst = cursor.execute('SELECT * FROM Maps').fetchall()
+            # uic.loadUi('test.ui', self)
+            self.setupUi(self)
+            for el in rqst:
+                pixmap = QPixmap()
+                pixmap.loadFromData(el[2], "png")
+                pixmap_resized = pixmap.scaled(120, 120, QtCore.Qt.KeepAspectRatio)
+                btn = QPushButton(el[1], self)
+                btn.setObjectName(el[3])
+                label = QLabel()
+                label.setPixmap(pixmap_resized)
+                frame = QVBoxLayout()
+                frame.addWidget(label, alignment=Qt.AlignCenter)
+                frame.addWidget(btn)
+                if btn.text() == 'The Island':
+                    btn.clicked.connect(lambda: self.open_map_window('The Island'))
+                elif btn.text() == 'Scorched Earth':
+                    btn.clicked.connect(lambda: self.open_map_window('Scorched Earth'))
+                elif btn.text() == 'Aberration':
+                    btn.clicked.connect(lambda: self.open_map_window('Aberration'))
+                elif btn.text() == 'Extinction':
+                    btn.clicked.connect(lambda: self.open_map_window('Extinction'))
+                elif btn.text() == 'Ragnarok':
+                    btn.clicked.connect(lambda: self.open_map_window('Ragnarok'))
+                self.MapButtons.addLayout(frame)
+        except Exception as e:
+            print(e)
 
     def open_map_window(self, btn_text):
         self.hide()
@@ -51,8 +64,67 @@ class MyArkManual(QMainWindow):
     def exit_from_app(self):
         self.close()
 
+    def open_searcher_window(self, user_input):
+        self.hide()
+        DinoSearch(edittext=user_input, parent=self)
 
-class Map(QMainWindow):
+
+class DinoSearch(QMainWindow, search_results.Ui_MainWindow):
+    def __init__(self, edittext, parent=None):
+        super().__init__(parent)
+        self.edittext = edittext
+        self.parent = parent
+        self.initUI()
+        self.show()
+        self.return_back.clicked.connect(self.previousWindow)
+
+    def initUI(self):
+        try:
+            self.setupUi(self)
+            rquest = 0
+            animal_frames = []
+            self.setupUi(self)
+            pixmap = QPixmap()
+            if self.edittext in FLAGS[0]:
+                rquest = cursor.\
+                    execute(f"""SELECT * FROM Creatures_info WHERE Temperament = '{self.edittext.lower()}'""").\
+                    fetchall()
+            elif self.edittext in FLAGS[1]:
+                rquest = cursor\
+                    .execute(f"""SELECT * FROM Creatures_info WHERE food_type = '{self.edittext.lower()}'""").fetchall()
+            elif self.edittext.lower() in FLAGS[2][0]:
+                rquest = cursor.execute(f"""SELECT * FROM Creatures_info WHERE rideable = 'Да'""").fetchall()
+            elif self.edittext.lower() in FLAGS[2][1]:
+                rquest = cursor.execute(f"""SELECT * FROM Creatures_info WHERE rideable = 'Нет'""").fetchall()
+            elif self.edittext.lower() not in FLAGS[0] and self.edittext.lower() not in FLAGS[1] and\
+                    self.edittext.lower() not in FLAGS[2] and len(self.edittext.lower()) > 0:
+                rquest = cursor.execute(f"""SELECT * FROM Creatures_info WHERE Name = '{self.edittext}'""").fetchall()
+            for el in rquest:
+                pixmap.loadFromData(el[15], "png")
+                pixmap_resized = pixmap.scaled(100, 100, QtCore.Qt.KeepAspectRatio)
+                dino_btn = QPushButton(el[1], self)
+                dino_btn.clicked.connect(lambda: self.open_dino_window(self.sender().text()))
+                label = QLabel()
+                label.setPixmap(pixmap_resized)
+                frame = QVBoxLayout()
+                frame.addWidget(label, alignment=Qt.AlignCenter)
+                frame.addWidget(dino_btn)
+                animal_frames.append(frame)
+            for x in range(1):
+                for y in range(3):
+                    self.gridLayout.addLayout(animal_frames[y], x, y)
+        except Exception as e:
+            print(e)
+
+    def open_dino_window(self, dino_btn_text):
+        Dino(current_dino=dino_btn_text, parent=self)
+
+    def previousWindow(self):
+        self.close()
+        self.parent.show()
+
+
+class Map(QMainWindow, test_map.Ui_MainWindow):
     def __init__(self, current_map, parent=None):
         super().__init__(parent)
         self.current_map = current_map
@@ -63,7 +135,7 @@ class Map(QMainWindow):
 
     def initUI(self):
         try:
-            uic.loadUi('test_map.ui', self)
+            self.setupUi(self)
             imgs = cursor.execute(f"""SELECT * FROM Map_regions WHERE map_name = '{self.current_map}'""").fetchall()
             pixmap = QPixmap()
             for el in imgs:
@@ -78,7 +150,8 @@ class Map(QMainWindow):
                 for i in range(len(regions)):
                     region_btn = QPushButton(regions[i], self)
                     region_btn.setGeometry(l2[i][0], l2[i][1], l2[i][2], l2[i][3])
-                    region_btn.clicked.connect(lambda: self.open_region_window(self.sender().text()))  # открываем окно региона, название которого соответствует тексту кнопки
+                    region_btn.clicked.connect(lambda: self.open_region_window(self.sender().text()))  # открываем окно
+                    # региона, название которого соответствует тексту кнопки
         except Exception as e:
             print(e)
 
@@ -90,7 +163,7 @@ class Map(QMainWindow):
         self.parent.show()
 
 
-class Region(QMainWindow):  # окно региона
+class Region(QMainWindow, region.Ui_MainWindow):  # окно региона
     def __init__(self, current_region, parent=None):
         super().__init__(parent)
         self.current_region = current_region
@@ -102,12 +175,15 @@ class Region(QMainWindow):  # окно региона
     def initUI(self):
         try:
             animal_frames = []
-            uic.loadUi('region.ui', self)
-            creatures = cursor.execute(f"""SELECT creatures FROM RegionsDinos WHERE region_name = '{self.current_region}'""").fetchall()  # выбираем из таблицы в базе данных нужные значения
+            self.setupUi(self)
+            creatures = cursor.\
+                execute(f"""SELECT creatures FROM RegionsDinos WHERE region_name = '{self.current_region}'""").\
+                fetchall()  # выбираем из таблицы в базе данных нужные значения
             animals = creatures[0][0].split(', ')
             pixmap = QPixmap()
             for dino in range(len(animals)):
-                icon = cursor.execute(f"""SELECT dino_icon FROM Creatures_info WHERE Name = '{animals[dino]}'""").fetchall()
+                icon = cursor.execute(f"""SELECT dino_icon FROM Creatures_info WHERE Name = '{animals[dino]}'""").\
+                    fetchall()
                 pixmap.loadFromData(icon[0][0], "png")
                 pixmap_resized = pixmap.scaled(100, 100, QtCore.Qt.KeepAspectRatio)
                 dino_btn = QPushButton(animals[dino], self)
@@ -118,8 +194,8 @@ class Region(QMainWindow):  # окно региона
                 frame.addWidget(label, alignment=Qt.AlignCenter)
                 frame.addWidget(dino_btn)
                 animal_frames.append(frame)
-            for x in range(1):
-                for y in range(2):
+            for x in range(3):
+                for y in range(6):
                     self.gridLayout.addLayout(animal_frames[y], x, y)
             self.title.setText(self.current_region)
         except Exception as e:
@@ -133,7 +209,7 @@ class Region(QMainWindow):  # окно региона
         Dino(current_dino=dino_btn_text, parent=self)
 
 
-class Dino(QMainWindow):
+class Dino(QMainWindow, DinoInfo.Ui_MainWindow):
     def __init__(self, current_dino, parent=None):
         super().__init__(parent)
         self.current_dino = current_dino
@@ -144,7 +220,7 @@ class Dino(QMainWindow):
 
     def initUI(self):
         try:
-            uic.loadUi('DinoInfo.ui', self)
+            self.setupUi(self)
             request = cursor.execute(f"""SELECT * FROM Creatures_info WHERE Name = '{self.current_dino}'""").fetchall()
             pixmap = QPixmap()
             pixmap1 = QPixmap()
@@ -157,6 +233,7 @@ class Dino(QMainWindow):
                 self.picture_2.setPixmap(pixmap_resized1)
                 self.title.setText(el[1])
                 self.tameable.setText(el[4])
+                self.temperament.setText(el[2])
                 self.food_type.setText(el[6])
                 self.rideable.setText(el[8])
                 self.mateable.setText(el[9])
@@ -183,5 +260,3 @@ if __name__ == '__main__':
     ex = MyArkManual()
     ex.show()
     sys.exit(app.exec())
-
-
